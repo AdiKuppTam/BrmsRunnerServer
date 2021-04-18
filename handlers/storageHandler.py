5e0c69bf00ad9b0025b014082b5b40ef30dc0e78
Adi Cooperman-Tamari <51295779+AdiCoopTam@users.noreply.github.com>
2021-04-18 16:51:47 +0300

    Initial API for storage handler

--- handlers/storageHandler.py
+++ handlers/storageHandler.py
@@ -1,6 +1,14 @@
+from pydrive.drive import GoogleDrive as Gd
+from pydrive.auth import GoogleAuth as Ga
+
+
 class StorageHandler:
+
     def __init__(self):
-        pass
+        gauth = Ga()
+        gauth.LocalWebserverAuth()
+        self.drive = Gd(gauth)
+        return
 
     def __new__(cls):
         if not hasattr(cls, 'instance'):
@@ -12,3 +20,43 @@ class StorageHandler:
 
     def delete_stimulus_of_experiment(self, experiment_name):
         pass
+
+    def upload_file(self, file_path):
+        """
+        upload a single file to google drive
+        :param file_path: the sile path
+        :return: True if success, False else
+        """
+        self.drive.CreateFile()
+
+    def upload_directory(self, directory_path):
+        """
+        upload a single file to google drive
+        :param directory_path: the sile path
+        :return: True if success, False else
+        """
+        self.drive.CreateFile()
+
+    def delete_file(self, file_name):
+        """
+        deletes a filename
+        :param file_name: the name of the file we are about to delete
+        :return: True if success, False else
+        """
+
+    def download_file(self, file_path):
+        """
+        download a single file to google drive
+        :param file_path: the sile path
+        :return: True if success, False else
+        """
+        self.drive.CreateFile()
+
+    def download_directory(self, directory_path):
+        """
+        download a single file to google drive
+        :param directory_path: the sile path
+        :return: True if success, False else
+        """
+        self.drive.CreateFile()
+
